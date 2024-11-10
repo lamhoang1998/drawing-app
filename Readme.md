ddb28b6eb260e36daf1924068ef5af4160a4c819
Lam <lamkyo373@gmail.com>
2024-11-10 23:11:01 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+-make an app with the canvas stretch the whole screen allowing user to drawing anywhere, but only rendering content at the middle inside the box.
+
+//innerWidth js property return the interior width of the window in px (the width of the window's layout viewport)
+//innerHeight js property return the interior height of the window in px
+
+-add event listener to the whole app
+-the canvas is positioned above everything in the app.
+-add point to our shapes anytime we click the mouse on the screen to draw.
+-finally add bezier curves to the app.
+-only collect point after clicking on the screen, not just basically moving or hovering the mouse over the screen => move others event into the callback function that handle the pointdown event.
+
+-only want to render shapes after the pointUp event fired, not just collecting all path and then keeping rendering them all after each event => create a shape array to collect all path after each pointUp event fired, and then give the "path" variable back to an empty array to collect new paths for next shapes to render new shapes after next pointUp events fired.
+
+-only want to render shapes but not let them stack onto each other after each rendering => just clear the previous rect and then recreate a new one before rendering a new shape.
---
+++ script.js
@@ -0,0 +1,79 @@
+const canvasProperties = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+	center: {
+		x: window.innerWidth / 2,
+		y: window.innerHeight / 2,
+	},
+};
+
+const stageProperties = {
+	width: 600,
+	height: 480,
+	left: canvasProperties.center.x - 600 / 2,
+	top: canvasProperties.center.y - 480 / 2,
+};
+
+myCanvas.width = canvasProperties.width;
+myCanvas.height = canvasProperties.height;
+
+const ctx = myCanvas.getContext("2d");
+clearCanvas();
+
+const shapes = [];
+let path = [];
+
+myCanvas.addEventListener("pointerdown", function (e) {
+	const mousePosition = {
+		x: e.offsetX,
+		y: e.offsetY,
+	};
+	path.push(mousePosition);
+
+	function moveCallback(e) {
+		const mousePosition = {
+			x: e.offsetX,
+			y: e.offsetY,
+		};
+		path.push(mousePosition);
+	}
+
+	function upCallback(e) {
+		myCanvas.removeEventListener("pointermove", moveCallback);
+		myCanvas.removeEventListener("pointerup", upCallback);
+
+		shapes.push(path);
+		path = [];
+
+		clearCanvas();
+		for (const shape of shapes) {
+			ctx.beginPath();
+			ctx.strokeStyle = "rgba(0,0,0,0.5)";
+			ctx.moveTo(shape[0].x, shape[0].y);
+			for (let i = 1; i < shape.length; i++) {
+				ctx.lineTo(shape[i].x, shape[i].y);
+			}
+
+			ctx.stroke();
+		}
+	}
+
+	myCanvas.addEventListener("pointermove", moveCallback);
+
+	myCanvas.addEventListener("pointerup", upCallback);
+});
+
+function clearCanvas() {
+	ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+
+	ctx.fillStyle = "gray";
+	ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
+
+	ctx.fillStyle = "white";
+	ctx.fillRect(
+		stageProperties.left,
+		stageProperties.top,
+		stageProperties.width,
+		stageProperties.height
+	);
+}
